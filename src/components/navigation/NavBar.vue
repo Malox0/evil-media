74d643f440f214f881b57e290c434858d7272f9f
Malox0 <maxi.busch02@gmail.com>
2025-10-02 23:36:28 +0200

    setup ts server, first structure, vuetify and navbar with mdi icons

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-toolbar title="Evil Media">
+    <template #append>
+      <v-btn icon="mdi-bell"></v-btn>
+
+      <v-btn icon="mdi-square-edit-outline"></v-btn>
+
+      <v-btn icon="mdi-account"></v-btn>
+    </template>
+  </v-toolbar>
+</template>
