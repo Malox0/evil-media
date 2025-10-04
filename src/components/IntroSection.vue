1cbce22966424b677129d444f5d64b59382a70d8
Malox0 <maxi.busch02@gmail.com>
2025-10-04 18:00:18 +0200

    fixed grid issue, improved btns

--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -17,8 +17,9 @@ function scrollToPosts() {
   </div>
   <h1 class="ma-6 text-h3 font-weight-bold text-center">Share Your bad Ideas with the World</h1>
   <p class="text-subtitle-1 text-medium-emphasis text-center">Share your trash.</p>
-  <v-btn class="ma-2" size="large">Get Started</v-btn>
-  <v-btn class="ma-2 mb-4" size="large" @click="scrollToPosts">Explore Posts</v-btn>
-
+  <div class="d-flex flex-row justify-center mt-4">
+    <v-btn class="ma-2 rounded-lg" size="large" color="primary">Get Started</v-btn>
+    <v-btn class="ma-2 mb-4 rounded-lg" size="large" @click="scrollToPosts">Explore Posts</v-btn>
+  </div>
   <v-divider class="mt-16" />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+
 import '@mdi/font/css/materialdesignicons.css'
 import { vuetify } from './plugins/vuetify'
 
