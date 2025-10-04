b761a24c9ddf9d8b09d0b3ef4f48f168e219d53c
Malox0 <maxi.busch02@gmail.com>
2025-10-04 14:36:13 +0200

    add listpost with post cards

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -2,9 +2,11 @@
 //Navbar
 import NavBar from '@/components/navigation/NavBar.vue'
 import IntroSection from '@/components/IntroSection.vue'
+import ListPosts from '@/components/ListPosts.vue'
 </script>
 <template>
   <NavBar />
 
   <IntroSection />
+  <ListPosts />
 </template>
---
+++ src/components/PostPreview.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  key: number
+  title: string
+  subTitle: string
+}>()
+</script>
+<template>
+  <v-card
+    variant="outlined"
+    color="surface-variant"
+    class="ma-8"
+    :subtitle="subTitle"
+    :title="title"
+  >
+    <template v-slot:actions>
+      <v-btn text="Button"></v-btn>
+    </template>
+  </v-card>
+</template>
